1e22cc46941cec55f1b68a47e1cede616453f86f
yli5 <yli5@caltech.edu>
2016-09-20 22:08:00 -0700

    add rphoton 4 p

--- feature_derive/feature_compute.py
+++ feature_derive/feature_compute.py
@@ -1,13 +1,16 @@
 # This file includes functions needed for feature process. 
 # These functions are used to calculate high-level features from row data,
 # and update in database
-
+#
+#After computing each feature, we will use Dataframe.join function to join features.
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 import matplotlib.pyplot as plt
 import psycopg2
 import csv
+
+
 
 
 def massdiff(record):
@@ -48,6 +51,7 @@ def massdiff(record):
             mass_diff[i] = max2_massdiff
     
     return mass_diff
+
 
 
 
@@ -126,4 +130,70 @@ def update_nlFromBrem(rawtable):
     return frame[['eid','nlfrombrem']].set_index('eid')
 
 
+
+
+
+
+def update_rphoton(rawtable):
+    """
+    This function updates 4-momentum of reconstructed photon, which is Pee - Pups.
+    """
+    conn = psycopg2.connect(database="darkphoton",user="yunxuanli")
+    cur_nl = conn.cursor()
+
+    cur_nl.execute("SELECT eid,eepx,eepy,eepz,eee,nups,upsp3,upscosth,upsphi,upsenergy FROM %s WHERE nups>0" % rawtable)
+    rows_nl = cur_nl.fetchall()
+    data_mc = np.array(rows_nl, dtype=object)
+    data = {'eid':data_mc[:,0],
+            'eepx':data_mc[:,1],
+            'eepy':data_mc[:,2],
+            'eepz':data_mc[:,3],
+            'eee':data_mc[:,4],
+            'nups':data_mc[:,5],
+            'upsp3':data_mc[:,6],
+            'upscosth':data_mc[:,7],
+            'upsphi':data_mc[:,8],
+            'upsenergy':data_mc[:,9],
+            'rphoton_px':Series(data_mc.shape[0]*[np.zeros(1)]),
+            'rphoton_py':Series(data_mc.shape[0]*[np.zeros(1)]),
+            'rphoton_pz':Series(data_mc.shape[0]*[np.zeros(1)]),
+            'rphoton_e':Series(data_mc.shape[0]*[np.zeros(1)])}
+    frame = DataFrame(data)
+
+    for i in range(frame.shape[0]):
+
+        result = frame.loc[i]
+        n = result['nups']
+
+        dp_px = np.zeros(n)
+        dp_py = np.zeros(n)
+        dp_pz = np.zeros(n)
+        dp_e = np.zeros(n)
+
+        for j in range(n):
+            ups_p3 = result['upsp3'][j]
+            ups_costh = result['upscosth'][j]
+            ups_phi = result['upsphi'][j]
+            ups_energy = result['upsenergy'][j]
+            
+            ups_px = ups_p3 * np.sqrt(1 - ups_costh**2) * np.cos(ups_phi)
+            ups_py = ups_p3 * np.sqrt(1 - ups_costh**2) * np.sin(ups_phi)
+            ups_pz = ups_p3 * ups_costh
+            
+            dp_px[j] = result['eepx'] - ups_px
+            dp_py[j] = result['eepy'] - ups_py
+            dp_pz[j] = result['eepz'] - ups_pz
+            dp_e[j]  = result['eee'] - ups_energy
+
+        result['rphoton_px'] = dp_px
+        result['rphoton_py'] = dp_py
+        result['rphoton_pz'] = dp_pz
+        result['rphoton_e']  = dp_e
+
+    # update to database
+    for i in range(frame.shape[0]):
+        result = frame.loc[i]
+        cur_nl.execute("UPDATE mcevent SET rphoton_px=%s,rphoton_py=%s,rphoton_pz=%s,rphoton_e=%s WHERE eid=%s",(result['rphoton_px'].tolist(),result['rphoton_py'].tolist(),result['rphoton_pz'].tolist(),result['rphoton_e'].tolist(),result['eid']))
+
+    return frame[['eid','rphoton_px','rphoton_py','rphoton_pz','rphoton_e']].set_index('eid')
 

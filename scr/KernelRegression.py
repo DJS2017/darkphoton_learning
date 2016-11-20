7d06e3ed7ed7fd47892658bf1dc279f7ec342489
yli5 <yli5@caltech.edu>
2016-11-19 19:45:37 -0800

    add set and get params in KR

--- scr/KernelRegression.py
+++ scr/KernelRegression.py
@@ -5,10 +5,11 @@ from scipy.spatial import cKDTree
 
 
 class KernelRegression:
-	"""
+    """
     This class aims to estimate unkown point's value using kernel methods.
-    parameter:
-	-----------
+    
+    Parameters
+    -----------
     x:  np.array
         input data points.
     
@@ -35,23 +36,117 @@ class KernelRegression:
     Examples
     -----------
     to be continued...
-	"""
+    """
+    
+    def __init__(self, n_neighbors=4, kernel_type='gaussian', bandwidth=0.01):
+        self.n_neighbors = n_neighbors
+        self.kernel_type = kernel_type
+        self.bandwidth = bandwidth
+        self.params = {'n_neighbors':self.n_neighbors,
+                       'kernel_type': self.kernel_type,
+                       'bandwidth' : self.bandwidth}
 
-	def __init__(self, x, y, n_neighbors=4, kernel='exp', bandwidth=0.01):
+
+
+    def fit(self, X, y):
+        self.tree = cKDTree(X)
+        self.values = y
+
+
+
+    def kernel(self, distance):
+        if(self.kernel_type == 'uniform'):
+            return 1.0/2 * (distance/self.bandwidth < 1.0)
+        elif(self.kernel_type == 'gaussian'):
+            return 1.0/np.sqrt(2*np.pi) * np.exp(-1.0/2 * (distance/self.bandwidth)**2)
+        else:
+            raise Exception("Invalid kernel type!", self.kernel_type)
+
+
+
+    def predict(self, X):
+        """Predict the values for the provided data
+        
+        Parameters
+        -----------
+        X: array-like, shape(n_query, n_features)
+
+        Returns
+        -----------
+        y: array of shape [n_samples]
+        """
+        
+        result = []
+        for x_ in X:
+            denominator = 0.0
+            numerator = 0.0
+            dist, neighbor_index = self.tree.query([x_],k=self.n_neighbors)
+            for i in range(len(dist[0])):
+                denominator = denominator + self.kernel(dist[0][i])
+                numerator = numerator + self.kernel(dist[0][i]) * self.values[neighbor_index[0][i]]
+            result.append(numerator/denominator)
+
+        return result
+
+    
+    def loss(self, X, y):
+        """Return the train loss
+
+        """
+        y_predict = self.predict(X)
+        return sum((self.values - y_predict)**2)/len(y_predict)
+
+
+    def score(self, X, y):
+        """Return the negative of loss as score.
+        This method is used as interface for grid search in scikit-learn.
+        """
+
+        return -1 * self.loss(X,y)
+
+
+    def get_params(self, deep=True):
+        """Get params for this estimator
+
+        Returns
+        ----------
+        params: dict of string to any.
+
+        """
+
+        result = {}
+        result['bandwidth'] = self.bandwidth
+        result['kernel_type'] = self.kernel_type
+        result['n_neighbors'] = self.n_neighbors
+        return result
+
+
+    def set_params(self, **params):
+        """Set params of this estimator
+        
+        """
+        valid_params = self.get_params()
+        for key, value in params.iteritems():
+            if(key in valid_params.keys()):
+                setattr(self, key, value)
+
+        return self
 
 
 
 
 
 
+
+"""
 class InverseKernelRegression(KernelRegression):
-    """
-    Specifies kernel regression when kernel is inverse functions.
-    """
+
+    #Specifies kernel regression when kernel is inverse functions.
+
 
     def __init__(self, x, y, n_neighbors=4, kernel='inverse', bandwidth=0.01,
                  order=2)
 
-
+"""
 
 
